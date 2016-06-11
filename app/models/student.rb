31df5f2752469808cfee2041b0c3b97a9f784d8d
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2016-06-11 09:01:10 +0530

    Fixing errors for heroku

--- app/models/student.rb
+++
@@ -1,7 +0,0 @@
-class Student < ActiveRecord::Base
-	has_secure_password
-
-	attr_accessible :email, :password
-
-	validates_uniqueness_of :email
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
 
-  resources :students
   resources :sessions
   get 'staff_section/index'
 

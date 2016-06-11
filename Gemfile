727c899eff295223f36fa55c5ad5c7fcaf6bf3f2
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2016-06-11 23:34:06 +0530

    Added mega-menu

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'coffee-rails', '~> 4.1.0'
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
 # Turbolinks makes following links in your web application faster. Read more: https://github.com/rails/turbolinks
-gem 'turbolinks'
+gem 'turbolinks', '>= 5.0.0.beta2'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 2.0'
 # bundle exec rake doc:rails generates the API under doc/api.
@@ -28,6 +28,8 @@ gem 'foundation-rails'
 #Font-Awesome
 
 gem 'font-awesome-rails'
+
+gem 'owlcarousel-rails'
 
 # Use ActiveModel has_secure_password
 gem 'bcrypt', '~> 3.1.7'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,13 +13,101 @@
 //= require jquery
 //= require jquery_ujs
 //= require foundation
+//= require owl.carousel
 //= require turbolinks
 //= require_tree .
 
-$(function () {
+$(function() {
+
+    $(".mega-menu").hide();
+
+    $(".mega-button").click(function() {
+        $(".mega-menu").toggle("slow");
+    })
     $(document).foundation();
 
-    $('.tertiary-nav .search-icon').click(function (event) {
+    var time = 7; // time in seconds
+
+    var $progressBar,
+        $bar,
+        $elem,
+        isPause,
+        tick,
+        percentTime;
+
+    //Init the carousel
+    $("#owl-demo").owlCarousel({
+        slideSpeed: 500,
+        paginationSpeed: 500,
+        singleItem: true,
+        afterInit: progressBar,
+        afterMove: moved,
+        startDragging: pauseOnDragging
+    });
+
+    //Init progressBar where elem is $("#owl-demo")
+    function progressBar(elem) {
+        $elem = elem;
+        //build progress bar elements
+        buildProgressBar();
+        //start counting
+        start();
+    }
+
+    //create div#progressBar and div#bar then prepend to $("#owl-demo")
+    function buildProgressBar() {
+        $progressBar = $("<div>", {
+            id: "progressBar"
+        });
+        $bar = $("<div>", {
+            id: "bar"
+        });
+        $progressBar.append($bar).prependTo($elem);
+    }
+
+    function start() {
+        //reset timer
+        percentTime = 0;
+        isPause = false;
+        //run interval every 0.01 second
+        tick = setInterval(interval, 10);
+    };
+
+    function interval() {
+        if (isPause === false) {
+            percentTime += 1 / time;
+            $bar.css({
+                width: percentTime + "%"
+            });
+            //if percentTime is equal or greater than 100
+            if (percentTime >= 100) {
+                //slide to next item 
+                $elem.trigger('owl.next')
+            }
+        }
+    }
+
+    //pause while dragging 
+    function pauseOnDragging() {
+        isPause = true;
+    }
+
+    //moved callback
+    function moved() {
+        //clear interval
+        clearTimeout(tick);
+        //start again
+        start();
+    }
+
+    $elem.on('mouseover', function() {
+        isPause = true;
+    })
+    $elem.on('mouseout', function() {
+        isPause = false;
+    })
+
+    $('.tertiary-nav .search-icon').click(function(event) {
 
         if ($('.inline-list li.search-field').hasClass('open')) {
 
@@ -41,7 +129,4 @@ $(function () {
 
         }
     });
-
-
-
 });

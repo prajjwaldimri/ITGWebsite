03628ac468d9798c9fe3254e6cf6147330d13834
Prajjwal Dimri <prajjwaldimri@hotmail.com>
2016-06-26 12:06:59 +0530

    Changed Footer

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
 
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: :destroy
+  before_action :admin_user, only: [:index, :destroy, :new]
   layout 'student_section'
 
   def index

40d96ad9a55ee7fe0856b112825f0867b5d007ea
Prajjwal <prajjwaldimri@hotmail.com>
2016-08-02 13:05:33 +0530

    Improved Staff and Student Section

--- app/controllers/student_section_controller.rb
+++ app/controllers/student_section_controller.rb
@@ -1,5 +1,8 @@
 class StudentSectionController < ApplicationController
   def index
     @user = current_user
+    @user_feed_items = Article.where(post_type: "Blog" || "Research").take(10)
+    @news_items = Article.where(post_type: "News").take(10)
+    @statement_items = Article.where(post_type: "Statement").take(10)
   end
 end

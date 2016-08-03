28c2fffb59458e7a48243d728285bfd18d99fd08
Prajjwal <prajjwaldimri@hotmail.com>
2016-08-03 21:26:44 +0530

    Fixing controller issues

--- app/controllers/departments_controller.rb
+++ app/controllers/departments_controller.rb
@@ -6,17 +6,32 @@ class DepartmentsController < ApplicationController
   end
 
   def ece
+    @user_feed_items = Article.where(post_type: "Blog" || "Research").take(10)
+    @news_items = Article.where(post_type: "News").take(10)
+    @statement_items = Article.where(post_type: "Statements").take(10)
   end
 
   def civil
+    @user_feed_items = Article.where(post_type: "Blog" || "Research").take(10)
+    @news_items = Article.where(post_type: "News").take(10)
+    @statement_items = Article.where(post_type: "Statements").take(10)
   end
 
   def mech
+    @user_feed_items = Article.where(post_type: "Blog" || "Research").take(10)
+    @news_items = Article.where(post_type: "News").take(10)
+    @statement_items = Article.where(post_type: "Statements").take(10)
   end
 
   def ee
+    @user_feed_items = Article.where(post_type: "Blog" || "Research").take(10)
+    @news_items = Article.where(post_type: "News").take(10)
+    @statement_items = Article.where(post_type: "Statements").take(10)
   end
 
   def hum
+    @user_feed_items = Article.where(post_type: "Blog" || "Research").take(10)
+    @news_items = Article.where(post_type: "News").take(10)
+    @statement_items = Article.where(post_type: "Statements").take(10)
   end
 end
